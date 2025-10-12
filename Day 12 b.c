a05f9504e6c5428bc53be4ac0569e36f30eceb4a
parthphutela <parthphutela58@gmail.com>
2025-10-13 00:24:03 +0530

    Add files via upload

---
+++ Day 12 b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        n /= 10;
+        count++;
+    }
+
+    printf("Number of digits = %d", count);
+    return 0;
+}
